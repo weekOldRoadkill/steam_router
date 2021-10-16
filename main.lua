c769850318f9ff09531ef7074b7292d0b21b3228
weekOldRoadkill <freddymushroom@gmail.com>
2021-10-16 13:17:23 -0400

    .

--- main.lua
+++ main.lua
@@ -12,18 +12,61 @@ local parser = argparse(
 )
 
 parser:argument("key", "Steam Web API key.")
+parser:argument("mode", "Determines whether it maps or paths."):choices({"map", "path"})
+parser:argument("file", "Map filename.")
 parser:argument("source", "Steam user ID to search from.")
 parser:argument("target", "Steam user ID to search for.")
+parser:argument("iterations", "Maximum search iterations. (default: 3)"):args("?")
 
 local args = parser:parse()
+args.iterations = tonumber(args.iterations) or 0x03
+
+
+
+
+-- Get Friends Function
+local count = 0x00
+local function get_friends(user)
+    local json
+    repeat
+        json = http.request("https://api.steampowered.com/ISteamUser/GetFriendList/v1/?key="..
+        args.key.."&steamid="..user)
+    until json
+
+    local friends = {}
+    for i in json:gmatch("\"%d+\"") do
+        friends[#friends+0x01] = i:sub(0x02, -0x02)
+    end
+
+    count = count+0x01
+    return friends
+end
 
 
 
 
 -- Code
-local json = http.request("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="..
-args.key.."&steamid="..args.source)
+if args.mode == "map" then
+    local users = {}
+    users[args.source] = get_friends(args.source)
 
-for id in json:gmatch("\"%d+\"") do
-    print(id:sub(0x02, -0x02))
+    for i = 0x01, args.iterations do
+        local temp = {}
+        for k, v in pairs(users) do temp[k] = v end
+
+        local done = false
+        for k, v in pairs(temp) do
+            for j = 0x01, #v do
+                users[v[j]] = users[v[j]] or get_friends(v[j])
+                if v[j] == args.target then done = true break end
+                io.write(count.."\r")
+                io.flush()
+            end
+        end
+        if done then break end
+    end
+
+    print(count)
+else
+    error("Path mode not implemented yet.")
 end
--- makefile
+++ makefile
@@ -3,8 +3,8 @@ all: .hererocks
 
 .hererocks:
 	python -m hererocks -j@v2.1 -r@ --compat=all .hererocks || pip install hererocks && python -m hererocks -j@v2.1 -r@ --compat=all .hererocks
-	.hererocks/bin/luarocks install luasocket
 	.hererocks/bin/luarocks install argparse
+	.hererocks/bin/luarocks install luasocket
 
 
 clean:
